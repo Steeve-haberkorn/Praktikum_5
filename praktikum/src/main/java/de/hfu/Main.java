0b9d5c2405cd6fade010a493664cf68d6253f3d1
Haberkorn Steeve <steeve.haberkorn@hs-furtwangen.de>
2021-11-11 13:01:58 +0100

    Praktikum commit

---
+++ praktikum/src/main/java/de/hfu/Main.java
@@ -0,0 +1,16 @@
+package de.hfu;
+
+import java.util.Scanner;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Veuillez saisir un mot :");
+		String str = sc.nextLine();
+		System.out.println("Vous avez saisi : " + str.toUpperCase());
+	}
+
+}
